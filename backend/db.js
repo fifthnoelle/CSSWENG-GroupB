a98da7e86eddbda4e61a42f63cd08a5d8a6877d8
fifthnoelle <70679663+fifthnoelle@users.noreply.github.com>
2026-06-15 21:05:05 +0800

    updated mongoDB connection

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGODB_URI || 'mongodb+srv://ricenroll:fQr9pDek3iMxTD0I@cluster0.jmti9v4.mongodb.net/?appName=Cluster0';
 
+/*
 const connectDB = async () => {
   try {
     await mongoose.connect(uri);
@@ -10,6 +12,28 @@ const connectDB = async () => {
     console.error("MongoDB Connection Error:", error);
     process.exit(1);
   }
-};
+};*/
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectDB() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 module.exports = connectDB;
